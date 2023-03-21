b8c4240aa053c3f3b70f4a42c21447b267fd0032
Manav Misra <manavm@visionwebsoft.com>
2023-03-20 21:42:09 -0500

    fix: actually delete the 📖

--- server/app/book/controller.js
+++ server/app/book/controller.js
@@ -11,24 +11,16 @@ const controller = {
     return Book.findOne({ bookId });
   },
   async delete(bookId, userId) {
-    console.log(
-      "🚀 ~ file: controller.js ~ line 20 ~ userId",
-      userId,
-      typeof userId
-    );
     // Does this  📖  belong to the logged in user?
     const bookToDelete = await this.show(bookId);
-
-    console.log(
-      "🚀 ~ file: controller.js ~ line 24 ~ bookToDelete",
-      bookToDelete
-    );
 
     if (bookToDelete.userId !== userId) {
       console.log(bookToDelete.userId, userId, "from the if statement");
       // TODO: Use Apollo Server 4 error handling
       throw new Error("You are not authorized to delete this book.");
     }
+
+    await Book.deleteOne({ bookId });
 
     return bookToDelete;
   },
