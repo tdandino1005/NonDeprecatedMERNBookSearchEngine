365b0742dac7f4ff63466ce8c7bbaf39ec7e9542
Manav Misra <manavm@visionwebsoft.com>
2023-03-21 10:58:46 -0500

    Update error.jsx

--- client/src/components/error.jsx
+++ client/src/components/error.jsx
@@ -1,23 +1,24 @@
 import PropTypes from "prop-types";
 import Alert from "react-bootstrap/Alert";
 
-export default function Error({ message, code }) {
+export default function Error({ error }) {
   return (
     <Alert variant="danger">
       <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-      <p>{message}</p>
+      <p>{error.message}</p>
       <hr />
-      <p className="mb-0">{code}</p>
+      <p className="mb-0">{error.code}</p>
     </Alert>
   );
 }
 
 Error.defaultProps = {
-  message: "An unknown error occurred.",
-  code: "UNKNOWN",
+  error: { message: "An unknown error occurred.", code: "UNKNOWN" },
 };
 
 Error.propTypes = {
-  message: PropTypes.string,
-  code: PropTypes.string,
+  error: PropTypes.shape({
+    message: PropTypes.string,
+    code: PropTypes.string,
+  }),
 };
