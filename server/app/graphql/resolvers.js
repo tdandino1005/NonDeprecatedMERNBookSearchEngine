79862e2d54664b26b444c4ad988f8b568d9e319a
Manav Misra <manavm@visionwebsoft.com>
2023-03-20 23:54:56 -0500

    feat: :goal_net: add some custom GraphQLError

--- server/app/graphql/resolvers.js
+++ server/app/graphql/resolvers.js
@@ -1,5 +1,19 @@
+import { ApolloServerErrorCode } from "@apollo/server/errors";
+import { GraphQLError } from "graphql";
 import bookController from "../book/controller.js";
 import userController from "../user/controller.js";
+
+function handleUnauthorizedError(error) {
+  if (error.extensions?.code !== ApolloServerErrorCode.GRAPHQL_PARSE_FAILED) {
+    throw new GraphQLError(error.message, {
+      extensions: {
+        code: "UNAUTHORIZED",
+      },
+    });
+  }
+
+  throw error;
+}
 
 export default {
   Query: {
@@ -11,19 +25,49 @@ export default {
   Mutation: {
     async createUser(_, { user }) {
       const token = await userController.create(user);
-
       return { token };
     },
     async login(_, { username, password }) {
+      if (!username || !password)
+        throw new GraphQLError("Username and password are required.", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+
       const token = await userController.show(username, password);
+
+      if (!token) handleUnauthorizedError(new Error("Invalid credentials."));
 
       return { token };
     },
     async saveBook(_, { book }, { user }) {
+      // Don't bother controller if no user
+      if (!user) {
+        throw new GraphQLError("You must be logged in to save 💾 a book.", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+
       return await bookController.create({ ...book, userId: user.id });
     },
     async removeBook(_, { bookId }, { user }) {
-      return await bookController.delete(bookId, user.id);
+      // Don't bother controller if no user
+      if (!user) {
+        throw new GraphQLError("You must be logged in to remove 🔥 a book.", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+
+      try {
+        return await bookController.delete(bookId, user.id);
+      } catch (error) {
+        handleUnauthorizedError(error);
+      }
     },
   },
 
