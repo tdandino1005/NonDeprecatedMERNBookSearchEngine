ebf28b4976d2ccd41cc1ca916603ab3f1a1bc041
Tony Dandino <tdandino1005@gmail.com>
2023-03-21 20:47:30 -0500

    Added Navbar text

--- client/src/components/header/navigation.jsx
+++ client/src/components/header/navigation.jsx
@@ -1,11 +1,12 @@
+import { CURRENT_USER } from "@/schema/type-defs";
+import { useQuery } from "@apollo/client";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import RegisterLogin from "../register-login/register-login";
+import RegisterLogin from "./register-login/register-login";
 
 export default function Navigation() {
-  //
   const [isShowingRegisterLogin, setIsShowingRegisterLogin] = useState(false);
 
   const { data } = useQuery(CURRENT_USER);
@@ -19,10 +20,17 @@ export default function Navigation() {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="nav" />
           <Navbar.Collapse className="justify-content-end" id="nav">
-            <Navbar.Text>
-              <Button onClick={() => setIsShowingRegisterLogin(true)}>
-                Login/Register
-              </Button>
+            <Navbar.Text className="text-white">
+              {data?.currentUser ? (
+                <>
+                  Signed in as: {data.currentUser.username}
+                  <Button className="ms-2">Logout</Button>
+                </>
+              ) : (
+                <Button onClick={() => setIsShowingRegisterLogin(true)}>
+                  Login/Register
+                </Button>
+              )}
             </Navbar.Text>
           </Navbar.Collapse>
         </Container>
