b811f79e8759c55a7a0177328692ec0bf52700d9
Manav Misra <manavm@visionwebsoft.com>
2023-03-21 20:05:33 -0500

    Submit the form and show the token in the response

--- client/src/components/register-login/register-login-form.jsx
+++ client/src/components/register-login/register-login-form.jsx
@@ -1,15 +1,29 @@
+import { LOGIN } from "@/schema/type-defs";
+import { useMutation } from "@apollo/client";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export default function RegisterLoginForm() {
+  const [login] = useMutation(LOGIN);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const username = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    console.log("sending out", username, password);
+    login({ variables: { username, password } });
+  };
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="email">
         <Form.Label>Username</Form.Label>
         <Form.Control type="text" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="password">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" />
       </Form.Group>
